569e7e282ebd7f5ea31b702e26800cae7a1e84cb
Volhan Salai <volhansalai@gmail.com>
2019-05-29 01:24:16 +0200

    video recording implemented

--- KNLVideo/RecordViewControllerKNL.swift
+++ KNLVideo/RecordViewControllerKNL.swift
@@ -7,26 +7,40 @@
 //
 
 import UIKit
+import MobileCoreServices
 
 class RecordViewControllerKNL: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func recordVideo(_ sender: Any) {
+        VideoKNL.startMediaBrowser(delegate: self, sourceType: .camera)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func video (_ videoPath: String, didFinishSavingWithError error: Error?, contextInfo info: AnyObject) {
+        let title = (error == nil) ? "Success" : "Error"
+        let message = (error == nil) ? "Video was saved" : "Video failed to save"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
-    */
+    
+}
 
+extension RecordViewControllerKNL: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        dismiss(animated: true, completion: nil)
+        
+        guard let mediaType = info[UIImagePickerController.InfoKey.mediaType] as? String, mediaType == (kUTTypeMovie as String), let url = info[UIImagePickerController.InfoKey.mediaURL] as? URL, UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(url.path) else { return }
+        
+        UISaveVideoAtPathToSavedPhotosAlbum(url.path, self, #selector(video(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+}
+
+extension RecordViewControllerKNL: UINavigationControllerDelegate {
+    
 }
