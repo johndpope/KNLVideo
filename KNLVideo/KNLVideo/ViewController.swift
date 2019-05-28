9e78c7e0758faccab4da200535316ed1b45e513c
Volhan Salai <volhansalai@gmail.com>
2019-05-28 23:28:08 +0200

    initial code commit

---
+++ KNLVideo/KNLVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KNLVideo
+//
+//  Created by Volhan Salai on 5/28/19.
+//  Copyright © 2019 Volhan Salai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
