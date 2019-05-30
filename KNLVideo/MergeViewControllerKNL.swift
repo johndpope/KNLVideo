47216e7596281945f91127ef56b530c7f2fd9601
Volhan Salai <volhansalai@gmail.com>
2019-05-30 03:59:21 +0200

    video merging final with high quality landscape video and merging with correct time ranges

--- KNLVideo/MergeViewControllerKNL.swift
+++ KNLVideo/MergeViewControllerKNL.swift
@@ -9,16 +9,118 @@
 import UIKit
 import MobileCoreServices
 import Photos
+import AVFoundation
 
 class MergeViewControllerKNL: UIViewController {
     var firstVideo: AVAsset?
     var secondVideo: AVAsset?
     var loadingFirstVideo = false
+    let hdVideoSize = CGSize(width: 1920.0, height: 1080.0)
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var activityMonitor: UIActivityIndicatorView!
+    @IBOutlet weak var firstVideoButton: UIButton!
+    @IBOutlet weak var secondVideoButton: UIButton!
+    @IBOutlet weak var animationButton: UIButton!
+    @IBOutlet weak var mergeButton: UIButton!
 
+    @IBAction func loadFirstVideo(_ sender: Any) {
+        if savedVideosAvailable() {
+            self.loadingFirstVideo = true
+            VideoKNL.startMediaBrowser(delegate: self, sourceType: .savedPhotosAlbum)
+        }
+    }
+    
+    @IBAction func loadSecondVideo(_ sender: Any) {
+        if savedVideosAvailable() {
+            loadingFirstVideo = false
+            VideoKNL.startMediaBrowser(delegate: self, sourceType: .savedPhotosAlbum)
+        }
+    }
+    
+    @IBAction func loadAnimation(_ sender: Any) {
+    }
+    
+    
+    @IBAction func merge(_ sender: Any) {
+        guard let firstVideo = self.firstVideo, let secondVideo = self.secondVideo else {
+            let alert = UIAlertController(title: "Videos not selected", message: "Please select both videos to merge them", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        self.processing()
+        
+        let composition = AVMutableComposition()
+        guard let firstTrack = composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid) else { return }
+        
+        do {
+            try firstTrack.insertTimeRange(CMTimeRangeMake(start: .zero, duration: firstVideo.duration), of: firstVideo.tracks(withMediaType: .video)[0], at: .zero)
+        } catch {
+            print("first track failed")
+            return
+        }
+        
+        guard let secondTrack = composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid) else { return }
+        
+        do {
+            try secondTrack.insertTimeRange(CMTimeRangeMake(start: .zero, duration: secondVideo.duration), of: secondVideo.tracks(withMediaType: .video)[0], at: firstVideo.duration)
+        } catch {
+            print("second track failed")
+            return
+        }
+        
+        
+        let mainInstruction = AVMutableVideoCompositionInstruction()
+        mainInstruction.timeRange = CMTimeRangeMake(start: .zero, duration: CMTimeAdd(firstVideo.duration, secondVideo.duration))
+        
+        let firstInstruction = VideoKNL.videoCompositionInstruction(firstTrack, asset: firstVideo)
+        firstInstruction.setOpacity(0.0, at: firstVideo.duration)
+        let secondInstruction = VideoKNL.videoCompositionInstruction(secondTrack, asset: secondVideo)
+        
+        mainInstruction.layerInstructions = [firstInstruction, secondInstruction]
+        let mainComposition = AVMutableVideoComposition()
+        mainComposition.instructions = [mainInstruction]
+        mainComposition.frameDuration = CMTimeMake(value: 1, timescale: 30)
+        mainComposition.renderSize = hdVideoSize
+        mainComposition.renderScale = 1.0
+        
+        self.applyAnimationToVideo(composition: mainComposition)
+        
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .short
+        let date = dateFormatter.string(from: Date())
+        let url = documentDirectory.appendingPathComponent("mergeVideo-\(date).mov")
+        
+        guard let exporter = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetHighestQuality) else { return }
+        exporter.outputURL = url
+        exporter.outputFileType = AVFileType.mov
+        exporter.shouldOptimizeForNetworkUse = true
+        exporter.videoComposition = mainComposition
+        
+        exporter.exportAsynchronously {
+            DispatchQueue.main.async {
+                self.exportDidFinish(session: exporter)
+            }
+        }
+    }
+    
+    func processing() {
+        if self.activityMonitor.isAnimating {
+            self.activityMonitor.stopAnimating()
+            self.firstVideoButton.isHidden = false
+            self.secondVideoButton.isHidden = false
+            self.animationButton.isHidden = false
+            self.mergeButton.isHidden = false
+        } else {
+            self.firstVideoButton.isHidden = true
+            self.secondVideoButton.isHidden = true
+            self.animationButton.isHidden = true
+            self.mergeButton.isHidden = true
+            self.activityMonitor.startAnimating()
+        }
     }
     
     func savedVideosAvailable() -> Bool {
@@ -34,6 +136,8 @@ class MergeViewControllerKNL: UIViewController {
     }
     
     func exportDidFinish(session: AVAssetExportSession) {
+        self.processing()
+        
         self.firstVideo = nil
         self.secondVideo = nil
         
@@ -63,77 +167,46 @@ class MergeViewControllerKNL: UIViewController {
             saveVideo()
         }
     }
-
-    @IBAction func loadFirstVideo(_ sender: Any) {
-        if savedVideosAvailable() {
-            self.loadingFirstVideo = true
-            VideoKNL.startMediaBrowser(delegate: self, sourceType: .savedPhotosAlbum)
-        }
-    }
-    @IBAction func loadSecondVideo(_ sender: Any) {
-        if savedVideosAvailable() {
-            loadingFirstVideo = false
-            VideoKNL.startMediaBrowser(delegate: self, sourceType: .savedPhotosAlbum)
-        }
-    }
-    @IBAction func loadAnimation(_ sender: Any) {
-    }
     
-    
-    @IBAction func merge(_ sender: Any) {
-        guard let firstVideo = self.firstVideo, let secondVideo = self.secondVideo else { return }
+    func applyAnimationToVideo(composition: AVMutableVideoComposition) {
+        let subtitleText = CATextLayer()
+//        subtitleText.font = "Superclarendon-Black"
+        subtitleText.fontSize = 72
+        subtitleText.frame = CGRect(x: 0.0, y: 76.0, width: hdVideoSize.width, height: 100.0)
+        subtitleText.string = "TEST"
+        subtitleText.alignmentMode = CATextLayerAlignmentMode.center
+        subtitleText.foregroundColor = UIColor.white.cgColor
+        subtitleText.shadowOpacity = 1.0
+        subtitleText.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        subtitleText.shadowRadius = 6.0
         
-        let composition = AVMutableComposition()
-        guard let firstTrack = composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid) else { return }
-        
-        do {
-            try firstTrack.insertTimeRange(CMTimeRangeMake(start: .zero, duration: firstVideo.duration), of: firstVideo.tracks(withMediaType: .video)[0], at: .zero)
-        } catch {
-            print("first track failed")
-            return
-        }
-        
-        
-        guard let secondTrack = composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid) else { return }
-        
-        do {
-            try secondTrack.insertTimeRange(CMTimeRangeMake(start: .zero, duration: secondVideo.duration), of: secondVideo.tracks(withMediaType: .video)[0], at: firstVideo.duration)
-        } catch {
-            print("second track failed")
-            return
-        }
+        let textOverlayLayer = CALayer()
+        textOverlayLayer.addSublayer(subtitleText)
+        textOverlayLayer.frame = CGRect(x: 0.0, y: 0.0, width: hdVideoSize.width, height: hdVideoSize.height)
         
         
-        let mainInstruction = AVMutableVideoCompositionInstruction()
-        mainInstruction.timeRange = CMTimeRangeMake(start: .zero, duration: CMTimeAdd(firstVideo.duration, secondVideo.duration))
+        let animateTitle = CABasicAnimation(keyPath: "transform.translation.x")
+        animateTitle.duration = 1.0
         
-        let firstInstruction = VideoKNL.videoCompositionInstruction(firstTrack, asset: firstVideo)
-        let secondInstruction = VideoKNL.videoCompositionInstruction(secondTrack, asset: secondVideo)
+        animateTitle.fromValue = -textOverlayLayer.frame.width
+        animateTitle.toValue = 0.0
+        animateTitle.beginTime = AVCoreAnimationBeginTimeAtZero
+        animateTitle.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        textOverlayLayer.add(animateTitle, forKey: "translation")
         
-        mainInstruction.layerInstructions = [firstInstruction, secondInstruction]
-        let mainComposition = AVMutableVideoComposition()
-        mainComposition.instructions = [mainInstruction]
-        mainComposition.frameDuration = CMTimeMake(value: 1, timescale: 60)
-        mainComposition.renderSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
         
-        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .long
-        dateFormatter.timeStyle = .short
-        let date = dateFormatter.string(from: Date())
-        let url = documentDirectory.appendingPathComponent("mergeVideo-\(date).mov")
+//        let videoLayer = CALayer()
+//        videoLayer.frame = CGRect(x: 0, y: 0, width: hdVideoSize.width - borderWidth * 2.0, height: hdVideoSize.height - borderWidth * 2.0)
         
-        guard let exporter = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetHighestQuality) else { return }
-        exporter.outputURL = url
-        exporter.outputFileType = AVFileType.mov
-        exporter.shouldOptimizeForNetworkUse = true
-        exporter.videoComposition = mainComposition
+        let parentLayer = CALayer()
+        parentLayer.frame = CGRect(x: 0.0, y: 0.0, width: hdVideoSize.width, height: hdVideoSize.height)
+        parentLayer.masksToBounds = true
         
-        exporter.exportAsynchronously {
-            DispatchQueue.main.async {
-                self.exportDidFinish(session: exporter)
-            }
-        }
+//        parentLayer.addSublayer(videoLayer)
+        parentLayer.addSublayer(textOverlayLayer)
+        
+//        composition.animationTool = AVVideoCompositionCoreAnimationTool(postProcessingAsVideoLayer: videoLayer, in: parentLayer)
+        composition.animationTool = AVVideoCompositionCoreAnimationTool(postProcessingAsVideoLayer: parentLayer, in: parentLayer)
     }
 }
 
--- KNLVideo/VideoKNL.swift
+++ KNLVideo/VideoKNL.swift
@@ -17,53 +17,22 @@ class VideoKNL {
         let mediaUI = UIImagePickerController()
         mediaUI.sourceType = sourceType
         mediaUI.mediaTypes = [kUTTypeMovie as String]
-        mediaUI.allowsEditing = true
+        mediaUI.allowsEditing = false
         mediaUI.delegate = delegate
+        mediaUI.videoQuality = .typeHigh
         delegate.present(mediaUI, animated: true, completion: nil)
     }
     
-    static func orientationFromTransform(_ transform: CGAffineTransform) -> (orientation: UIImage.Orientation, isPortrait: Bool) {
-        var assetOrientation = UIImage.Orientation.up
-        var isPortrait = false
-        if transform.a == 0 && transform.b == 1.0 && transform.c == -1.0 && transform.d == 0 {
-            assetOrientation = .right
-            isPortrait = true
-        } else if transform.a == 0 && transform.b == -1.0 && transform.c == 1.0 && transform.d == 0 {
-            assetOrientation = .left
-            isPortrait = true
-        } else if transform.a == 1.0 && transform.b == 0 && transform.c == 0 && transform.d == 1.0 {
-            assetOrientation = .up
-        } else if transform.a == -1.0 && transform.b == 0 && transform.c == 0 && transform.d == -1.0 {
-            assetOrientation = .down
-        }
-        return (assetOrientation, isPortrait)
-    }
-    
     static func videoCompositionInstruction(_ track: AVCompositionTrack, asset: AVAsset) -> AVMutableVideoCompositionLayerInstruction {
+        
         let instruction = AVMutableVideoCompositionLayerInstruction(assetTrack: track)
         let assetTrack = asset.tracks(withMediaType: AVMediaType.video)[0]
         
-        let transform = assetTrack.preferredTransform
-        let assetInfo = orientationFromTransform(transform)
+        let scaleToFitRatio = 1920.0 / assetTrack.naturalSize.width
         
-        var scaleToFitRatio = UIScreen.main.bounds.width / assetTrack.naturalSize.width
-        if assetInfo.isPortrait {
-            scaleToFitRatio = UIScreen.main.bounds.width / assetTrack.naturalSize.height
-            let scaleFactor = CGAffineTransform(scaleX: scaleToFitRatio, y: scaleToFitRatio)
-            instruction.setTransform(assetTrack.preferredTransform.concatenating(scaleFactor), at: .zero)
-        } else {
-            let scaleFactor = CGAffineTransform(scaleX: scaleToFitRatio, y: scaleToFitRatio)
-            var concat = assetTrack.preferredTransform.concatenating(scaleFactor)
-                .concatenating(CGAffineTransform(translationX: 0, y: UIScreen.main.bounds.width / 2))
-            if assetInfo.orientation == .down {
-                let fixUpsideDown = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
-                let windowBounds = UIScreen.main.bounds
-                let yFix = assetTrack.naturalSize.height + windowBounds.height
-                let centerFix = CGAffineTransform(translationX: assetTrack.naturalSize.width, y: yFix)
-                concat = fixUpsideDown.concatenating(centerFix).concatenating(scaleFactor)
-            }
-            instruction.setTransform(concat, at: .zero)
-        }
+        let scaleFactor = CGAffineTransform(scaleX: scaleToFitRatio, y: scaleToFitRatio)
+        let concat = assetTrack.preferredTransform.concatenating(scaleFactor)
+        instruction.setTransform(concat, at: .zero)
         
         return instruction
     }
